895e0237d3e53d3551c038c8699d4fc82474cc1e
ed wassermann <vis0vis@gmail.com>
2017-04-10 20:19:47 +0800

    First draft codomo dummy

--- source/style/js/scripts.js
+++ source/style/js/scripts.js
@@ -1,21 +1,187 @@
 $(document).ready(function() {
     'use strict';
     /*-----------------------------------------------------------------------------------*/
-    /*	LOADER
-    /*-----------------------------------------------------------------------------------*/
-    $(".pageloader").lsPreloader({
-
-        backgroundColor: "#212121",
-        logoImage: "style/images/logo.png",
-        minimumTime: .5,
-        progressHeight: "0",
-        progressColor: "#fff",
-        percentFontSize: "18px"
-
+    /*	LIGHTGALLERY
+	/*-----------------------------------------------------------------------------------*/
+    $('.light-gallery').lightGallery({
+        thumbnail: false,
+        selector: '.lgitem',
+        animateThumb: true,
+        showThumbByDefault: false,
+        download: false,
+        autoplayControls: false,
+        zoom: false,
+	    fullScreen: false,
+        thumbWidth: 100,
+        thumbContHeight: 80,
+        videoMaxWidth: '1000px'
     });
+    /*-----------------------------------------------------------------------------------*/
+    /*	IMAGE ICON HOVER
+    /*-----------------------------------------------------------------------------------*/
+    $('.overlay a').prepend('<span class="over"><span></span></span>');
+    /*-----------------------------------------------------------------------------------*/
+    /*	PROGRESS BAR
+    /*-----------------------------------------------------------------------------------*/
+    $('.progress-list .progress .bar').progressBar({
+        shadow: false,
+        percentage: false,
+        animation: true,
+        height: 12
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TAB COLLAPSE
+    /*-----------------------------------------------------------------------------------*/
+    $('#tab1').tabCollapse({
+        tabsClass: 'hidden-sm hidden-xs',
+        accordionClass: 'visible-sm visible-xs'
+    });
+    $('#tab1').on('shown-accordion.bs.tabcollapse', function() {
+        $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+        $('.panel-group').on('shown.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').addClass(' panel-active');
+        }).on('hidden.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').removeClass(' panel-active');
+        });
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOGGLE
+    /*-----------------------------------------------------------------------------------*/
+    $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+    $('.panel-group').on('shown.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').addClass(' panel-active');
+    }).on('hidden.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').removeClass(' panel-active');
+    });
+    
+    /*-----------------------------------------------------------------------------------*/
+    /*	PRETTIFY
+    /*-----------------------------------------------------------------------------------*/
+    window.prettyPrint && prettyPrint();
+    /*-----------------------------------------------------------------------------------*/
+    /*	DATA REL
+    /*-----------------------------------------------------------------------------------*/
+    $('a[data-rel]').each(function() {
+        $(this).attr('rel', $(this).data('rel'));
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOOLTIP
+    /*-----------------------------------------------------------------------------------*/
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip();
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	LAZY LOAD GOOGLE MAPS
+    /*-----------------------------------------------------------------------------------*/
+    ;
+    (function($, window, document, undefined) {
+        var $window = $(window),
+            mapInstances = [],
+            $pluginInstance = $('.google-map').lazyLoadGoogleMaps({
+                callback: function(container, map) {
+                    var $container = $(container),
+                        center = new google.maps.LatLng($container.attr('data-lat'), $container.attr('data-lng'));
+
+                    map.setOptions({
+                        center: center,
+                        zoom: 15,
+                        zoomControl: true,
+                        zoomControlOptions: {
+                            style: google.maps.ZoomControlStyle.DEFAULT,
+                        },
+                        disableDoubleClickZoom: false,
+                        mapTypeControl: true,
+                        mapTypeControlOptions: {
+                            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                        },
+                        scaleControl: true,
+                        scrollwheel: false,
+                        streetViewControl: true,
+                        draggable: true,
+                        overviewMapControl: false,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    });
+                    new google.maps.Marker({
+                        position: center,
+                        map: map
+                    });
+
+                    $.data(map, 'center', center);
+                    mapInstances.push(map);
+
+                    var updateCenter = function() {
+                        $.data(map, 'center', map.getCenter());
+                    };
+                    google.maps.event.addListener(map, 'dragend', updateCenter);
+                    google.maps.event.addListener(map, 'zoom_changed', updateCenter);
+                    google.maps.event.addListenerOnce(map, 'idle', function() {
+                        $container.addClass('is-loaded');
+                    });
+                }
+            });
+
+        $window.on('resize', $pluginInstance.debounce(1000, function() {
+            $.each(mapInstances, function() {
+                this.setCenter($.data(this, 'center'));
+            });
+        }));
+
+    })(jQuery, window, document);
     /*-----------------------------------------------------------------------------------*/
     /*	VANILLA
     /*-----------------------------------------------------------------------------------*/
     var myForm;
     myForm = new VanillaForm($("form.vanilla-form"));
+    /*-----------------------------------------------------------------------------------*/
+    /*	INSTAGRAM
+    /*-----------------------------------------------------------------------------------*/
+    var instagramFeed = new Instafeed({
+        target: 'instafeed-full',
+        get: 'user',
+        limit: 8,
+        userId: 1215763826,
+        accessToken: '1215763826.f1627ea.dad6ca96bd7642519b573d52c3ef467f',
+        resolution: 'standard_resolution',
+        template: '<div class="item col-sm-3"><figure class="overlay instagram"><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></figure></div>',
+        after: function() {
+            $('#instafeed-full figure.overlay a').prepend('<span class="over"><span></span></span>');
+        }
+    });
+    $('#instafeed-full').each(function() {
+        instagramFeed.run();
+    }); 
+    /*-----------------------------------------------------------------------------------*/
+    /*	COUNTER UP
+    /*-----------------------------------------------------------------------------------*/
+    $('.fcounter').counterUp({
+        delay: 50,
+        time: 1000
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COLLAGEPLUS
+    /*-----------------------------------------------------------------------------------*/
+    collage();
+
+
+    function collage() {
+        $('#collage-medium').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 300,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+    };
+    
+    
+    
+    // This is just for the case that the browser window is resized
+    var resizeTimer = null;
+    $(window).on('resize', function() {
+        // hide all the images until we resize them
+        $('.collage .collage-image-wrapper').css("opacity", 0);
+        // set a timer to re-apply the plugin
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collage, 200);
+    });
 });
